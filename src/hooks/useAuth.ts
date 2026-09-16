68b8f7baa17d8117b92ab1da0c63f41bb54b67d5
Youssef Mohamed Fekry Abdelghafar Nasr <yousseffekryk98@gmail.com>
2026-09-16 07:19:23 +0300

    refactor: move auth hook out of provider module

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,1 +1,10 @@
-export { useAuth } from '../context/AuthContext'
+import { useContext } from 'react'
+import { AuthContext } from '../context/auth-context'
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used inside AuthProvider')
+  }
+  return context
+}
